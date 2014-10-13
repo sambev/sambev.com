5be215833f9f97c10b12804009d22d8834c45c1c
Sam Beveridge <sam.bev87@gmail.com>
2014-10-12 22:34:13 -0600

    Added titlecase filter

--- static/js/reports/modules/reports.js
+++ static/js/reports/modules/reports.js
@@ -1,1 +1,11 @@
 var reportApp = angular.module('reportApp', []);
+
+// Thanks to http://ng.malsup.com/#!/titlecase-filter for the filter
+reportApp.filter('titlecase', function() {
+    return function(s) {
+        s = ( s === undefined || s === null ) ? '' : s;
+        return s.toString().toLowerCase().replace( /\b([a-z])/g, function(ch) {
+            return ch.toUpperCase();
+        });
+    };
+});
